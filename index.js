3a8c6c652c862d81fd26b6f664fc396f77c79199
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-11-26 17:00:50 +0600

    jwt

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+ function verifyJWT(req, res, next){
+    // console.log('token', req.headers.authorization);
+    const authHeader =  req.headers.authorization
+    if(!authHeader){
+        return res.status(401).send('unauthorized access')
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(error, decoded){
+        if(error){
+            return res.status(403).send({message: 'forbiden access'})
+        }
+        req.decoded = decoded
+        next()
+    } )
+
+ }
 
 
 async function run() {
@@ -59,9 +75,14 @@ async function run() {
         
        })
 
-       app.get('/bookings', async(req, res) => {
+       app.get('/bookings',verifyJWT, async(req, res) => {
         const email = req.query.email
+        const decodedEmail = req.decoded.email
+        if(email !== decodedEmail){
+            return res.status(403).send({message: 'forbidden access'})
+        }
         const query = {email: email}
+
         const result = await bookingsCollection.find(query).toArray()
         res.send(result)
        })
