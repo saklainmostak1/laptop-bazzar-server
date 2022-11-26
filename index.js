8fea538a5531b13afc13bac6ef915685c5a075cc
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-11-26 23:39:03 +0600

    delete user

--- index.js
+++ index.js
@@ -155,6 +155,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/users/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result = await usersCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
 
 
