2a7ca869df2cc8db6478293ee2b6b94a2c22a2f9
saklainmostak1 <saklain15-12003@diu.edu.bd>
2022-11-26 16:06:59 +0600

    token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const cors = require('cors');
 const port = process.env.PORT || 5000
@@ -63,6 +64,18 @@ async function run() {
         const query = {email: email}
         const result = await bookingsCollection.find(query).toArray()
         res.send(result)
+       })
+
+       app.get('/jwt', async(req, res) =>{
+            const email = req.query.email
+            const query = {email: email}
+            const user = await usersCollection.findOne(query)
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1h'})
+                return res.send({accessToken: token})
+            }
+          
+            res.status(403).send({accessToken: ''})
        })
 
 
